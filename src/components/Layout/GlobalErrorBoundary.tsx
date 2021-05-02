156eff41ee5c536f08d2a4853e27fa4697097f55
CaicoLeung <lkk1053190902@gmail.com>
2021-05-02 23:38:56 +0800

    feat: 添加ErrorBoundary组件, 对页面报错捕获, 防止页面空白

---
+++ src/components/Layout/GlobalErrorBoundary.tsx
@@ -0,0 +1,42 @@
+import If from '#Comp/ControlStament/If'
+import { PreWrap } from '#Comp/StyledComponents'
+import { Button, Result } from 'antd'
+import React, { ErrorInfo } from 'react'
+
+interface GlobalErrorBoundaryState {
+  error: Error | null;
+  stack: string;
+}
+
+export default class GlobalErrorBoundary extends React.Component<unknown, GlobalErrorBoundaryState> {
+  state = {
+    error: null,
+    stack: ''
+  }
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    this.setState({
+      error,
+      stack: errorInfo.componentStack
+    })
+  }
+
+  render() {
+    return (
+      <If
+        condition={this.state.error}
+        fallback={this.props.children}
+      >
+        <Result
+          status="error"
+          title="组件内部报错了!"
+          subTitle={<PreWrap>{this.state.stack}</PreWrap>}
+          extra={
+            <Button type="primary" key="console" onClick={location.reload}>
+              刷新
+            </Button>
+          }
+        />
+      </If>
+    )
+  }
+}
