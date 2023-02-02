a230012149f6683c87743cf008ed4a20d5d3ba0f
CaicoLeung <lkk1053190902@gmail.com>
2023-02-02 15:53:35 +0800

    feat: 用useReducer 代替 useState

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,50 +1,63 @@
-import React, { useState } from 'react'
+import React, { useReducer } from 'react'
 import logo from '#Images/home/logo.svg'
 import ComA from '#Comp/ComA'
 import ComB from '#Comp/ComB'
-import { Button, Input, Row, Space } from 'antd'
+import { Button, Col, Input, Row } from 'antd'
 import './index.less'
 import { Link } from 'react-router-dom'
 import { AlibabaOutlined } from '@ant-design/icons'
 
 const Home: React.FC = () => {
-  const [count, setCount] = useState(0);
-  const [count2, setCount2] = useState(0);
-  const [text, setText] = useState<string>('');
-  const [errorState, setErrorState] = useState<any>({ message: 'Hello Vite + React!' });
+  const initState = {
+    count: 0,
+    count2: 0,
+    text: '',
+    errorState: 'Hello Vite + React!',
+  }
 
-  const errorHandle = () => setErrorState(errorState.error);
+  type State = typeof initState;
 
-  const appTitle = import.meta.env.VITE_APP_TITLE;
-  const appEnv = import.meta.env.VITE_CUSTOM_ENV;
-  const isProd = import.meta.env.PROD ? 'YES' : 'NO';
-  const isDev = import.meta.env.DEV ? 'YES' : 'NO';
+  const reducer = (prev: State, next: Partial<State>) => ({ ...prev, ...next })
+
+  const [state, dispatch] = useReducer(reducer, initState);
+
+  const errorHandle = () => dispatch({ errorState: state.errorState.error });
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{errorState.message}</p>
+        <p>{state.errorState}</p>
         <Row gutter={[4, 8]} justify="center">
-          <Link to="/test">
-            <Button type="primary" icon={<AlibabaOutlined />}>
-              去ahooks页
+          <Col span={12}>
+            <Link to="/test">
+              <Button type="primary" icon={<AlibabaOutlined />}>
+                去ahooks页
+              </Button>
+            </Link>
+          </Col>
+          <Col span={12}>
+            <Button danger onClick={errorHandle}>测试页面报错崩溃</Button>
+          </Col>
+          <Col span={8}>
+            <Button type="primary" onClick={() => dispatch({count: state.count + 1})}>
+              count is: {state.count}
             </Button>
-          </Link>
-          <Button danger onClick={errorHandle}>测试页面报错崩溃</Button>
-          <Button type="primary" onClick={() => setCount(() => count + 1)}>
-            count is: {count}
-          </Button>
-          <Button type="primary" danger onClick={() => setCount2(() => count2 + 1)}>
-            count2 is: {count2}
-          </Button>
-          <Input type="text" placeholder="组件B的文本内容" value={text} onChange={value => setText(value.target.value)} />
+          </Col>
+          <Col span={8}>
+            <Button type="primary" danger onClick={() => dispatch({count2: state.count2 + 1})}>
+              count2 is: {state.count2}
+            </Button>
+          </Col>
+          <Col span={8}>
+            <Input type="text" placeholder="组件B的文本内容" value={state.text} onChange={({target}) => dispatch({text: target.value})} />
+          </Col>
         </Row>
         <p>
           F12打开控制台查看输出信息
         </p>
-        <ComA count={count} />
-        <ComB count={count2} text={text} />
+        <ComA count={state.count} />
+        <ComB count={state.count2} text={state.text} />
       </header>
     </div>
   );
